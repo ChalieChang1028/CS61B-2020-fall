0f12d41e6d44f26600079ecdb8b3d38f2cd79fbe
Ming Cheng <mingchengprc@gmail.com>
2020-08-26 22:44:51 -0700

    proj0 complete

---
+++ proj0/Body.java
@@ -0,0 +1,68 @@
+public class Body{
+  public double xxPos, yyPos, xxVel, yyVel, mass, G=6.67e-11;
+  public String imgFileName;
+  public Body(double xP, double yP, double xV, double yV,
+              double m, String img){
+    xxPos = xP;
+    yyPos = yP;
+    xxVel = xV;
+    yyVel = yV;
+    mass = m;
+    imgFileName = img;
+  }
+  public Body(Body b){
+    xxPos = b.xxPos;
+    yyPos = b.yyPos;
+    xxVel = b.xxVel;
+    yyVel = b.yyVel;
+    mass = b.mass;
+    imgFileName = b.imgFileName;
+  }
+
+  public double calcDistance(Body b){
+    return Math.sqrt(Math.pow(xxPos-b.xxPos, 2)+Math.pow(yyPos-b.yyPos, 2));
+  }
+
+  public double calcForceExertedBy(Body b){
+    if (b.xxPos==xxPos && b.yyPos==yyPos) return 0.0;
+    return G*mass*b.mass/Math.pow(calcDistance(b), 2);
+  }
+
+  public double calcForceExertedByX(Body b){
+    if (b.xxPos==xxPos && b.yyPos==yyPos) return 0.0;
+    return calcForceExertedBy(b)*(b.xxPos-xxPos)/calcDistance(b);
+  }
+
+  public double calcForceExertedByY(Body b){
+    if (b.xxPos==xxPos && b.yyPos==yyPos) return 0.0;
+    return calcForceExertedBy(b)*(b.yyPos-yyPos)/calcDistance(b);
+  }
+
+  public double calcNetForceExertedByX(Body[] allb){
+    double ans = 0;
+    for(Body b : allb){
+      ans += calcForceExertedByX(b);
+    }
+    return ans;
+  }
+
+  public double calcNetForceExertedByY(Body[] allb){
+    double ans = 0;
+    for(Body b : allb){
+      ans += calcForceExertedByY(b);
+    }
+    return ans;
+  }
+
+  public void update(double dt, double fx, double fy){
+    xxVel += dt*fx/mass;
+    yyVel += dt*fy/mass;
+    xxPos += dt*xxVel;
+    yyPos += dt*yyVel;
+  }
+
+  public void draw(){
+    StdDraw.picture(xxPos, yyPos, "images/"+imgFileName);
+  }
+
+}
---
+++ proj0/NBody.java
@@ -0,0 +1,64 @@
+public class NBody{
+
+  public static void main(String[] args) {
+    double T = Double.parseDouble(args[0]), dt = Double.parseDouble(args[1]), time = 0;
+    String filename = args[2];
+    double R = readRadius(filename);
+    Body[] allBodies = readBodies(filename);
+
+    StdDraw.enableDoubleBuffering();
+    StdDraw.setScale(-R, R);
+    StdDraw.clear();
+    StdDraw.picture(0, 0, "images/starfield.jpg");
+
+    for (Body b: allBodies){
+      b.draw();
+    }
+
+    while (time < T){
+      Double[] xForces = new Double[allBodies.length];
+      Double[] yForces = new Double[allBodies.length];
+
+      StdDraw.setScale(-R, R);
+      StdDraw.clear();
+      StdDraw.picture(0, 0, "images/starfield.jpg");
+
+      for (int i = 0; i < allBodies.length; i++){
+        xForces[i] = allBodies[i].calcNetForceExertedByX(allBodies);
+        yForces[i] = allBodies[i].calcNetForceExertedByY(allBodies);
+        allBodies[i].update(dt, xForces[i], yForces[i]);
+        allBodies[i].draw();
+      }
+      StdDraw.show();
+      StdDraw.pause(10);
+      time += dt;
+    }
+    StdOut.printf("%d\n", allBodies.length);
+    StdOut.printf("%.2e\n", R);
+    for (int i = 0; i < bodies.length; i++) {
+      StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
+                allBodies[i].xxPos, allBodies[i].yyPos, allBodies[i].xxVel,
+                  allBodies[i].yyVel, allBodies[i].mass, allBodies[i].imgFileName);
+}
+  }
+
+  public static double readRadius(String fname){
+    In in = new In(fname);
+    in.readDouble();
+    return in.readDouble();
+  }
+
+  public static Body[] readBodies(String fname){
+    In in = new In(fname);
+    int n = in.readInt();
+    in.readDouble();
+    Body[] allb = new Body[n];
+    for(int i = 0; i < n; i++)
+    {
+      allb[i] = new Body(in.readDouble(), in.readDouble(), in.readDouble(),
+                      in.readDouble(), in.readDouble(), in.readString());
+    }
+    return allb;
+  }
+
+}
