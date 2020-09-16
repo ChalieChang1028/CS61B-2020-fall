8ac1f3527ab614de8b6c932a9f30964324c65e3f
Ming Cheng <mingchengprc@gmail.com>
2020-09-16 15:55:20 -0700

    proj1b g

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -1,6 +1,5 @@
 import static org.junit.Assert.*;
 import org.junit.Test;
-
 import java.nio.charset.StandardCharsets;
 
 public class TestArrayDequeGold {
@@ -8,17 +7,30 @@ public class TestArrayDequeGold {
     @Test
     public void testDeque() {
         StudentArrayDeque<Integer> sarr = new StudentArrayDeque<>();
+        ArrayDequeSolution<Integer> sol = new ArrayDequeSolution<>();
         int i = 0;
-        while (i < 25){
-            double randomNum = StdRandom.uniform();
-            if (randomNum < 0.33) {
-                sarr.addLast(i);
-            } else if (randomNum < 0.66 && ) {
-                sarr.addFirst(i);
-            }
-            else {
+        String record = "";
 
+        while (i < 100){
+            double randomNum = StdRandom.uniform();
+            if (randomNum <= 0.25) {
+                sarr.addLast(i);
+                sol.addLast(i);
+                record += "addLast(" + i + ")\n";
+            } else if (randomNum <= 0.5 && sarr.size() > 0) {
+                Integer stu = sarr.removeFirst(), act = sol.removeFirst();
+                record += "removeFirst()\n";
+                assertEquals(record, stu, act);
+            } else if (randomNum <= 0.75 && sarr.size() > 0) {
+                Integer stu = sarr.removeLast(), act = sol.removeLast();
+                record += "removeLast()\n";
+                assertEquals(record, stu, act);
+            } else {
+                sarr.addFirst(i);
+                sol.addFirst(i);
+                record += "addFirst(" + i + ")\n";
             }
+            i++;
         }
     }
 }
