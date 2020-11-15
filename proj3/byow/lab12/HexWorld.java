7f180d8ef169a5047753ebe3f00c898b9ed1d9d8
Ming Cheng <mingchengprc@gmail.com>
2020-11-15 15:32:55 -0800

    proj3 part1

--- proj3/byow/lab12/HexWorld.java
+++ proj3/byow/lab12/HexWorld.java
@@ -14,9 +14,9 @@ import java.util.Random;
 public class HexWorld {
 
     private static final int WIDTH = 60;
-    private static final int HEIGHT = 45;
+    private static final int HEIGHT = 40;
 
-    private static final long SEED = 19990724; // MY B-DAY
+    private static final long SEED = 99999999;
     private static final Random RANDOM = new Random(SEED);
 
 
@@ -46,28 +46,22 @@ public class HexWorld {
             addLine(side - 1, x + 1, y, world, t);
         }
     }
+
     public static void main(String[] args) {
         // initialize the tile rendering engine with a window of size WIDTH x HEIGHT
         TERenderer ter = new TERenderer();
         ter.initialize(WIDTH, HEIGHT);
 
         // initialize tiles
-        TETile background = Tileset.FLOOR;
+        TETile background = Tileset.NOTHING;
         TETile[][] world = new TETile[WIDTH][HEIGHT];
         for (int x = 0; x < WIDTH; x += 1) {
             for (int y = 0; y < HEIGHT; y += 1) {
                 world[x][y] = background;
             }
         }
-        //drawChar(world, 5, 2, 5, Tileset.WATER);
-        //drawLine(world, 0, 0, 2, 1, Tileset.WATER);
-        addHexagon(4, 0, 0, world, Tileset.WATER);
-        //singleHexagon(world, 0, 8, 4, Tileset.WATER);
-        //singleHexagon(world, 10 - 3, 4, 4, Tileset.FLOWER);
-        //drawRandomVerticalHexes(world, 3, 0, 0, 3);
-        //generateHexagon(world, 4);
-        // by column, 3-4-5-4-3, respectively
 
+        addHexagon(4, 0, 0, world, Tileset.WATER);
         ter.renderFrame(world);
     }
 }
